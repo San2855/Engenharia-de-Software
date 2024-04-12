280ccf5c4b199d833362b02cd6d6c5885516d5d1
Santiago Henrique <105302316+San2855@users.noreply.github.com>
2024-04-12 12:06:01 +0000

    Adição de mais casos de teste e criação da classe calculator

--- test_calculator.py
+++ test_calculator.py
@@ -8,9 +8,11 @@ class TestCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(3, 5), 8)
         self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 7), 3)
         self.assertEqual(self.calc.subtract(5, 5), 0)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
         self.assertEqual(self.calc.subtract(0, 0), 0)
