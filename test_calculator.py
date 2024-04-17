a6921e051a03e01cb6b95655027e19b26206a069
Lucas Sotero <105369580+lucasoteroo@users.noreply.github.com>
2024-04-17 00:43:19 +0000

    commit test multiplicação

--- test_calculator.py
+++ test_calculator.py
@@ -17,13 +17,6 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-2, -2), 0)
         self.assertEqual(self.calc.subtract(0, 0), 0)
         
-    def test_divisao(self):
-        self.assertEqual(self.calc.divisao(10,2),5)
-        self.assertEqual(self.calc.divisao(8,2),4)
-        self.assertEqual(self.calc.divisao(18,3),6)
-        self.assertEqual(self.calc.divisao(25,5),5)
-        self.assertEqual(self.calc.divisao(36,6),6)
-
     def test_multip(self):
         self.assertEqual(self.calc.multip(10,2),20)
         self.assertEqual(self.calc.multip(14,2),28)
