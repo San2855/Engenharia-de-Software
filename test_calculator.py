a72b7ab7124837bfa4b180abd5e1fe91e96d0eb9
Lucas Sotero <105369580+lucasoteroo@users.noreply.github.com>
2024-04-17 01:19:56 +0000

    teste sobre a multiplicação criado

--- test_calculator.py
+++ test_calculator.py
@@ -19,10 +19,10 @@ class TestCalculator(unittest.TestCase):
         
     def test_multip(self):
         self.assertEqual(self.calc.multip(0,2),0)
-        self.assertEqual(self.calc.multip(10,2),20)
-        self.assertEqual(self.calc.multip(14,2),28)
-        self.assertEqual(self.calc.multip(82,3),246)
-        self.assertEqual(self.calc.multip(31,5),155)
+        self.assertEqual(self.calc.multip(-10,2),-20)
+        self.assertEqual(self.calc.multip(-14,-2),28)
+        self.assertEqual(self.calc.multip(82,-3),-246)
+        self.assertEqual(self.calc.multip(31,0),0)
         self.assertEqual(self.calc.multip(36,6),216)
         
 if __name__ == "__main__":
