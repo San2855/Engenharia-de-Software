478ad03108e8799688def82f32a78873656be727
Jvctr <102014411+Velky07@users.noreply.github.com>
2024-04-18 16:56:49 +0000

    Implementado testes de caso para função de divisão

--- test_calculator.py
+++ test_calculator.py
@@ -24,6 +24,18 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multip(82,-3),-246)
         self.assertEqual(self.calc.multip(31,0),0)
         self.assertEqual(self.calc.multip(36,6),216)
+
+    def test_div(self):
+        self.assertEqual(self.calc.div(0,2), 0)
+        self.assertEqual(self.calc.div(-16,2), -8)
+        self.assertEqual(self.calc.div(-15,-2), 7.5)
+        self.assertEqual(self.calc.div(81,-3), -27)
+        self.assertEqual(self.calc.div(49,7), 7)
+        self.assertEqual(self.calc.div(36,0), None)
+        self.assertEqual(self.calc.div(0,0), None)
+        self.assertEqual(self.calc.div(6,6), 1)  
+        self.assertEqual(self.calc.div(50,2), 25)
+        self.assertEqual(self.calc.div(36.9,2), 18.45)
         
 if __name__ == "__main__":
     unittest.main()
